b26a211dffb83ae372b2584ab4f1e2c0174e5cef
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-03-23 13:53:27 +0100

    rating added

--- lib/main.dart
+++ lib/main.dart
@@ -56,9 +56,11 @@ class _MyAppState extends State<MyApp> {
               ),
             ],
           ),
-          body: Column(children: <Widget>[
-            ProductScreenTopPart(),
-          ],)),
+          body: Column(
+            children: <Widget>[
+              ProductScreenTopPart(),
+            ],
+          )),
     );
   }
 }
@@ -75,28 +77,64 @@ class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       width: double.infinity,
-       height: screenAwareSize(245, context),
-       child: Stack(
-         children: <Widget>[
-           Container(
-             child: Image.asset("assets/adidas.png", width: double.infinity, height: double.infinity, fit: BoxFit.contain,),
-           ),
-
-           Align(
-             alignment: Alignment.topRight,
-             child: Container(
-               width: screenAwareSize(50.0, context),
-               height: screenAwareSize(50.0, context),
-               decoration: BoxDecoration(
-                 color: Colors.black26,
-                 shape: BoxShape.circle,
-               ),
-               child: Image.asset("assets/360.png"),
-             ),
-           )
-         ],
-       ),
+      width: double.infinity,
+      height: screenAwareSize(245, context),
+      child: Stack(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                child: Image.asset(
+                  "assets/adidas.png",
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0, right: 35.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: screenAwareSize(50.0, context),
+                    height: screenAwareSize(50.0, context),
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.asset("assets/360.png"),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Positioned(
+            left: screenAwareSize(18.0, context),
+            bottom: screenAwareSize(15.0, context),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Rating",
+                  style: TextStyle(
+                      color: Color(0xFF949598),
+                      fontSize: screenAwareSize(14.0, context),
+                      fontFamily: "Monteserrat-SemiBold"),
+                ),
+                SizedBox(height: screenAwareSize(8.0, context)),
+                Row(
+                  children: <Widget>[
+                    SizedBox(width: 8.0,),
+                    Icon(Icons.star, color: Color(0xFFFFE600),),
+                    Text("4.5", style: TextStyle(color:Color(0xFFFFE600)),),
+                    SizedBox(width: screenAwareSize(5.0, context),),
+                    Text("(378 People)", style: TextStyle(color: Colors.white)),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
